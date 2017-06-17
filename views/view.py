7b24b41effff62ced728c531d1bb22430628816c
hacker <286537741@qq.com>
2017-06-17 17:54:33 +0800

    后台分页

--- views/view.py
+++ views/view.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding=utf-8
-from flask import Flask,request,render_template,redirect,url_for
+from flask import Flask,request,render_template,redirect,url_for,jsonify
 from lib.mongo import MongoDB 
-from lib.query_logic import query_logic 
-import re 
+from lib.query_logic import query_logic   
+from urllib import parse 
+import re  
+import json
 import pdb
 
 app = Flask(__name__) 
@@ -19,12 +21,12 @@ def index():
 
 @app.route('/result.html',methods=['POST','GET'])
 def result(): 
-    q = request.args.get('q','')
-    q = q.strip().split(';')
-    query = query_logic(q)
-    data = mongo.coll.find(query).sort([('ip',1),('port',1)])
+#    q = request.args.get('q','')
+#    q = q.strip().split(';')
+#    query = query_logic(q)
+#    data = mongo.coll.find(query).sort([('ip',1),('port',1)])
 
-    return render_template('result.html',data=data,q=q)
+    return render_template('result.html') 
 
 
 @app.route('/search.html',methods=['POST','GET'])
@@ -33,8 +35,54 @@ def search():
 
 @app.route('/test.html')
 def test():
-    data = mongo.coll.find().sort([('ip',1),('port',1)])
-    return render_template('test.html',data=data)
+    return render_template('test.html')
+
+@app.route('/data.json',methods=['POST','GET'])
+def json():       
+    referer = request.headers['Referer']    
+    if referer.find('result') != -1:
+        parse_result = parse.urlparse(referer) 
+        param_dict = parse.parse_qs(parse_result.query)  
+        try:
+            q = param_dict['q'][0]  
+            q = q.strip().split(';') 
+        except:
+            q = ['']
+    else: 
+        q = ['']  
+
+    query = query_logic(q)
+    draw = request.form.get('draw')
+    start =  request.form.get('start')
+    length = request.form.get('length')    
+    draw = int(draw)
+    start = int(start)
+    length = int(length)  
+
+    print('draw:{}  start:{}  length:{}  '.format(draw,start,length))
+
+    recordsTotal = mongo.coll.count()  
+    recordsFiltered = mongo.coll.find(query).count() 
+    data = mongo.coll.find(query,projection={'_id':False}).sort([('ip',1),('port',1)]).skip(start).limit(length)    
+    dd = []
+    for d in data:  
+        tmp = [] 
+        tmp.append(d['ip']) 
+        tmp.append(d['port']) 
+        tmp.append(d['date']) 
+        tmp.append(d['banner'][:100])
+        dd.append(tmp)
+
+
+    result = {
+        'draw':draw,
+        'recordsTotal':recordsTotal,
+        'recordsFiltered':recordsFiltered,
+        'data':dd
+    } 
+
+    return jsonify(result)
+
 
 
 if __name__ == '__main__':
